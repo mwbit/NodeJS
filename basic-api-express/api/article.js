6701e949f04ed34ccbf8e92c3683a92b9e503fa3
Marcelo <mwbitt@gmail.com>
2019-03-23 16:29:26 -0300

    update article remove function

--- basic-api-express/api/article.js
+++ basic-api-express/api/article.js
@@ -38,7 +38,11 @@ module.exports = app => {
         .db("articles")
         .where({ id: req.params.id })
         .del();
-      notExistsOrError(rowsDeleted, "Artigo não encontrado.");
+      try {
+        notExistsOrError(rowsDeleted, "Artigo não encontrado.");
+      } catch (err) {
+        return res.status(400).send(msg);
+      }
       res.status(204).send();
     } catch (msg) {
       res.status(500).send(msg);
